18c6c597336a6b28c6afd6a53920e177069b7a90
zero9178 <markus.boeck02@gmail.com>
2021-01-02 23:02:20 +0100

    Generate assembly in the same order as the basic blocks and insert gotos after conditional jumps if needed

--- Micro16C.Backend/GenAssembly.fs
+++ Micro16C.Backend/GenAssembly.fs
@@ -17,9 +17,6 @@ let genAssembly (irModule: Module): AssemblyLine list =
 
     let prependOperation operation list = (Operation operation) :: list
 
-    let mutable visitedBlocks =
-        ImmutableHashSet.Create<Value ref>(HashIdentity.Reference)
-
     let genPhi instr list =
         match (!instr).ParentBlock with
         | None -> failwith "Internal Compiler Error: Instruction with no parent block"
@@ -49,131 +46,139 @@ let genAssembly (irModule: Module): AssemblyLine list =
                                 list
                     | _ -> failwith "Internal Compiler Error: Expected Phi Instruction") list) list
 
-    let rec genAssemblyForBlock list bbValue =
-        if visitedBlocks.Contains bbValue then
-            list
-        else
-            visitedBlocks <- visitedBlocks.Add(bbValue)
-            let bb = !bbValue |> Value.asBasicBlock
+    let basicBlocks =
+        irModule |> Module.basicBlocks |> Array.ofList
+
+    basicBlocks
+    |> Array.indexed
+    |> Array.fold (fun list (bbIndex, bbValue) ->
+        let bb = !bbValue |> Value.asBasicBlock
+
+        let list =
+            (!bbValue |> Value.name |> Label) :: list
+
+        bb
+        |> BasicBlock.instructions
+        |> List.fold (fun list instr ->
 
             let list =
-                (!bbValue |> Value.name |> Label) :: list
+                if Value.isTerminating !instr then genPhi instr list else list
 
+            match !instr with
+            | { Content = BinaryInstruction { Left = Ref { Content = Constant { Value = 0x8000s } }
+                                              Right = op
+                                              Kind = And } }
+            | { Content = BinaryInstruction { Right = Ref { Content = Constant { Value = 0x8000s } }
+                                              Left = op
+                                              Kind = And } } ->
+                if operandToBus instr = operandToBus op then
+                    list
+                else
+                    prependOperation
+                        { Operation.Default with
+                              SBus = operandToBus instr |> Some
+                              ABus = operandToBus op |> Some
+                              AMux = Some AMux.ABus
+                              ALU = Some ALU.ABus
+                              Shifter = Some Shifter.Noop }
+                        list
+            | { Content = BinaryInstruction { Left = lhs; Right = rhs; Kind = kind } } ->
+                prependOperation
+                    { Operation.Default with
+                          AMux = Some AMux.ABus
+                          SBus = instr |> operandToBus |> Some
+                          ABus = lhs |> operandToBus |> Some
+                          BBus = rhs |> operandToBus |> Some
+                          ALU = Some(if kind = Add then ALU.Add else ALU.And)
+                          Shifter = Some Shifter.Noop }
+                    list
+            | { Content = UnaryInstruction { Value = value; Kind = Not } } ->
+                prependOperation
+                    { Operation.Default with
+                          AMux = Some AMux.ABus
+                          SBus = instr |> operandToBus |> Some
+                          ABus = value |> operandToBus |> Some
+                          ALU = Some ALU.Neg
+                          Shifter = Some Shifter.Noop }
+                    list
+            | { Content = UnaryInstruction { Value = value; Kind = Shl } } ->
+                prependOperation
+                    { Operation.Default with
+                          AMux = Some AMux.ABus
+                          SBus = instr |> operandToBus |> Some
+                          ABus = value |> operandToBus |> Some
+                          ALU = Some ALU.ABus
+                          Shifter = Some Shifter.Left }
+                    list
+            | { Content = UnaryInstruction { Value = value; Kind = Shr } } ->
+                prependOperation
+                    { Operation.Default with
+                          AMux = Some AMux.ABus
+                          SBus = instr |> operandToBus |> Some
+                          ABus = value |> operandToBus |> Some
+                          ALU = Some ALU.ABus
+                          Shifter = Some Shifter.Left }
+                    list
+            | { Content = GotoInstruction { BasicBlock = branch } } ->
 
-            bb
-            |> BasicBlock.instructions
-            |> List.fold (fun list instr ->
-
+                if Some branch
+                   <> Array.tryItem (bbIndex + 1) basicBlocks then
+                    prependOperation
+                        { Operation.Default with
+                              Address = !branch |> Value.name |> Some
+                              Condition = Some Cond.None }
+                        list
+                else
+                    list
+            | { Content = CondBrInstruction { Condition = Ref { Content = BinaryInstruction { Kind = And
+                                                                                              Right = Ref { Content = Constant { Value = 0x8000s } } } } as value
+                                              FalseBranch = falseBranch
+                                              TrueBranch = trueBranch } }
+            | { Content = CondBrInstruction { Condition = Ref { Content = BinaryInstruction { Kind = And
+                                                                                              Left = Ref { Content = Constant { Value = 0x8000s } } } } as value
+                                              FalseBranch = falseBranch
+                                              TrueBranch = trueBranch } } ->
                 let list =
-                    if Value.isTerminating !instr then genPhi instr list else list
-
-                match !instr with
-                | { Content = BinaryInstruction { Left = Ref { Content = Constant { Value = 0x8000s } }
-                                                  Right = op
-                                                  Kind = And } }
-                | { Content = BinaryInstruction { Right = Ref { Content = Constant { Value = 0x8000s } }
-                                                  Left = op
-                                                  Kind = And } } ->
-                    if operandToBus instr = operandToBus op then
-                        list
-                    else
-                        prependOperation
-                            { Operation.Default with
-                                  SBus = operandToBus instr |> Some
-                                  ABus = operandToBus op |> Some
-                                  AMux = Some AMux.ABus
-                                  ALU = Some ALU.ABus
-                                  Shifter = Some Shifter.Noop }
-                            list
-                | { Content = BinaryInstruction { Left = lhs; Right = rhs; Kind = kind } } ->
                     prependOperation
                         { Operation.Default with
+                              Address = !trueBranch |> Value.name |> Some
                               AMux = Some AMux.ABus
-                              SBus = instr |> operandToBus |> Some
-                              ABus = lhs |> operandToBus |> Some
-                              BBus = rhs |> operandToBus |> Some
-                              ALU = Some(if kind = Add then ALU.Add else ALU.And)
-                              Shifter = Some Shifter.Noop }
-                        list
-                | { Content = UnaryInstruction { Value = value; Kind = Not } } ->
-                    prependOperation
-                        { Operation.Default with
-                              AMux = Some AMux.ABus
-                              SBus = instr |> operandToBus |> Some
-                              ABus = value |> operandToBus |> Some
-                              ALU = Some ALU.Neg
-                              Shifter = Some Shifter.Noop }
-                        list
-                | { Content = UnaryInstruction { Value = value; Kind = Shl } } ->
-                    prependOperation
-                        { Operation.Default with
-                              AMux = Some AMux.ABus
-                              SBus = instr |> operandToBus |> Some
+                              Condition = Some Cond.Neg
                               ABus = value |> operandToBus |> Some
                               ALU = Some ALU.ABus
-                              Shifter = Some Shifter.Left }
+                              Shifter = None }
                         list
-                | { Content = UnaryInstruction { Value = value; Kind = Shr } } ->
+
+                if Some falseBranch = Array.tryItem (bbIndex + 1) basicBlocks then
+                    list
+                else
                     prependOperation
                         { Operation.Default with
+                              Address = !falseBranch |> Value.name |> Some
+                              Condition = Some Cond.None }
+                        list
+            | { Content = CondBrInstruction { Condition = value
+                                              FalseBranch = falseBranch
+                                              TrueBranch = trueBranch } } ->
+                let list =
+                    prependOperation
+                        { Operation.Default with
+                              Address = !falseBranch |> Value.name |> Some
                               AMux = Some AMux.ABus
-                              SBus = instr |> operandToBus |> Some
+                              Condition = Some Cond.Zero
                               ABus = value |> operandToBus |> Some
                               ALU = Some ALU.ABus
-                              Shifter = Some Shifter.Left }
+                              Shifter = None }
                         list
-                | { Content = GotoInstruction { BasicBlock = branch } } ->
-                    if visitedBlocks.Contains branch then
-                        prependOperation
-                            { Operation.Default with
-                                  Address = !branch |> Value.name |> Some
-                                  Condition = Some Cond.None }
-                            list
-                    else
-                        genAssemblyForBlock list branch
-                | { Content = CondBrInstruction { Condition = Ref { Content = BinaryInstruction { Kind = And
-                                                                                                  Right = Ref { Content = Constant { Value = 0x8000s } } } } as value
-                                                  FalseBranch = falseBranch
-                                                  TrueBranch = trueBranch } }
-                | { Content = CondBrInstruction { Condition = Ref { Content = BinaryInstruction { Kind = And
-                                                                                                  Left = Ref { Content = Constant { Value = 0x8000s } } } } as value
-                                                  FalseBranch = falseBranch
-                                                  TrueBranch = trueBranch } } ->
-                    let list =
-                        prependOperation
-                            { Operation.Default with
-                                  Address = !trueBranch |> Value.name |> Some
-                                  AMux = Some AMux.ABus
-                                  Condition = Some Cond.Neg
-                                  ABus = value |> operandToBus |> Some
-                                  ALU = Some ALU.ABus
-                                  Shifter = None }
-                            list
 
-                    let list = genAssemblyForBlock list falseBranch
-                    genAssemblyForBlock list trueBranch
-                | { Content = CondBrInstruction { Condition = value
-                                                  FalseBranch = falseBranch
-                                                  TrueBranch = trueBranch } } ->
-                    let list =
-                        prependOperation
-                            { Operation.Default with
-                                  Address = !falseBranch |> Value.name |> Some
-                                  AMux = Some AMux.ABus
-                                  Condition = Some Cond.Zero
-                                  ABus = value |> operandToBus |> Some
-                                  ALU = Some ALU.ABus
-                                  Shifter = None }
-                            list
-
-                    let list = genAssemblyForBlock list trueBranch
-                    genAssemblyForBlock list falseBranch
-                | { Content = PhiInstruction _ } -> list
-                | _ -> list) list
-
-    match irModule
-          |> Module.basicBlocks
-          |> List.tryHead
-          |> Option.map (genAssemblyForBlock []) with
-    | None -> []
-    | Some l -> l |> List.rev
+                if Some trueBranch = Array.tryItem (bbIndex + 1) basicBlocks then
+                    list
+                else
+                    prependOperation
+                        { Operation.Default with
+                              Address = !trueBranch |> Value.name |> Some
+                              Condition = Some Cond.None }
+                        list
+            | { Content = PhiInstruction _ } -> list
+            | _ -> list) list) []
+    |> List.rev
